707951a3ebdf37548cb95914d2d07d4ad2752a5b
scollo26 <giovanniscollo26@gmail.com>
2022-02-02 15:40:55 +0100

    main con foto album

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,4 @@
 <header>
-    <img src="./img/spotify.png" alt="logo" class="logo" />
+    <!-- <img src="./img/spotify.png" alt="logo" class="logo" /> -->
+    <img class="logo" src="https://cdn.discordapp.com/attachments/895657298859544588/938427657585037352/spotify.png" alt="">
 </header>
--- partials/main.php
+++ partials/main.php
@@ -9,7 +9,17 @@ include_once __DIR__ . "/../server/db.php";
 <div class="container ">
     <div class="flex">
 
-    ciao
+        <?php 
+        foreach ($albums as $disc) {
+    
+        ?>
+        <div class="card-container">
+            <img class="card-img" src="<?= $disc['poster']?>" alt="">
+            <h4 class="card-title"><?= $disc['title']?></h4>
+            <h5 class="card-author"><?= $disc['author']?></h5>
+            <p class="card-year"><?= $disc['year']?></p>
+        </div>
+        <?php } ?>
 
     </div>
 </div>
